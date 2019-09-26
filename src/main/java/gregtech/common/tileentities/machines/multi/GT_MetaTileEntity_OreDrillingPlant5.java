02e9475ca8167d2e637d02aceba37f8dbe9f15bc
observeroftime02 <observeroftime@shizuki.ca>
2019-09-26 13:30:01 -0700

    Maybe...? Fix Weebium casings compatability issue with GT++?

--- src/main/java/gregtech/common/tileentities/machines/multi/GT_MetaTileEntity_OreDrillingPlant5.java
+++ src/main/java/gregtech/common/tileentities/machines/multi/GT_MetaTileEntity_OreDrillingPlant5.java
@@ -39,7 +39,7 @@ public class GT_MetaTileEntity_OreDrillingPlant5 extends GT_MetaTileEntity_OreDr
 
     @Override
     protected int getCasingTextureIndex() {
-        return 65;
+        return 127;
     }
 
     @Override
